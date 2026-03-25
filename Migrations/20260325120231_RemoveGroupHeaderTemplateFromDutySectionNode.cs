77f8c5345c88032d9558311afc711c055cf61ef6
Andrii Fomin <andrew.fomin@gmail.com>
2026-03-25 15:33:04 +0200

    бд фикс

---
+++ Migrations/20260325120231_RemoveGroupHeaderTemplateFromDutySectionNode.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Base2.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveGroupHeaderTemplateFromDutySectionNode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GroupHeaderTemplate",
+                table: "DutySectionNodes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GroupHeaderTemplate",
+                table: "DutySectionNodes",
+                type: "TEXT",
+                maxLength: 500,
+                nullable: true);
+        }
+    }
+}
